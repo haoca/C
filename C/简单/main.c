99227ec0573491f560b4eef05367c3a573bd2033
haoca <1612573657@qq.com>
2020-04-07 21:13:42 +0800

    2020.4.7

---
+++ C/简单/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=0,b,c,d,i,j,k,m,n;
+    char mode1,mode2;
+    printf("[1]请选择您当前数的类型：\nA.10进制\nB.2进制\nC.8进制\nD.16进制\n");
+    scanf("%c",&mode1);
+    printf("[1]请选择您目标数的类型：\nA.10进制\nB.2进制\nC.8进制\nD.16进制\n");
+    scanf("%c",&mode2);
+    if(mode1==65){
+
+    }
+    else if(mode1==66){
+
+    }
+    else if(mode1==67){
+
+    }
+    else if(mode1==68){
+
+    }
+    return 0;
+}
